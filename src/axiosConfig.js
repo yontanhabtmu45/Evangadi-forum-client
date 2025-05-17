b950c1ace4f8ca41fdc1e7b88ea93df28cab0dbe
Jhan-hab <201286123+yontanhabtmu45@users.noreply.github.com>
2025-05-17 13:59:51 +0300

    finally

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const axiosBase = axios.create({
+    // local
     // baseURL: 'http://localhost:5700/api'
+    
+    // production
     baseURL: 'https://evangadi-forum-server-r645.onrender.com/api'
 })
 export default axiosBase
